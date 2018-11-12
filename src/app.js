683b2b614fa8a4d88c4d3424ccf77fd8ef47813a
Git Dong <939929159@qq.com>
2018-11-12 11:23:08 +0800

    files

--- src/app.js
+++ src/app.js
@@ -6,26 +6,29 @@ const path = require('path');
 const fs = require('fs');
 const server = http.createServer((req, res) => {
   const filePath = path.join(conf.root, req.url);
-  fs.stat(filePath, (err, stat) => {
+  fs.stat(filePath, (err, stats) => {
     if (err) {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('404 Not Found');
+      res.end(`${filePath} is NOT FOUND`);
       return;
     }
-    if (stat.isFile) {
-      res.writeHead(200, { 'Content—Type': 'text/plain' });
+    if (stats.isFile()) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       fs.createReadStream(filePath).pipe(res);
-    } else if(stat.isDirectory) {
+      return;
+    } else if (stats.isDirectory()) {
       fs.readdir(filePath, (err, filse) => {
-        if(err) {
-          res.writeHead(200, { 'Content—Type': 'text/plain' });
-          res.end(filse.join(','));
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(err);
         }
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(filse.join(','));
       });
     }
   });
 
 });
 server.listen(conf.port, conf.hostname, () => {
-  log(chalk.green(`server running at http//${conf.hostname}:${conf.port}`));
+  log(chalk.green(`server running at http://${conf.hostname}:${conf.port}`));
 });
