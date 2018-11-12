401aaaf8908e2cb28a1ab5639e4cc8487042a7ad
Git Dong <939929159@qq.com>
2018-11-12 14:05:02 +0800

    promisify

--- src/route/route.js
+++ src/route/route.js
@@ -4,7 +4,7 @@ const { promisify } = require('util');
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 
-module.export = async function (req, res, filePath) {
+module.exports = async function (req, res, filePath) {
   try {
     const stats = await stat(filePath);
     if (stats.isFile()) {
@@ -12,11 +12,12 @@ module.export = async function (req, res, filePath) {
       fs.createReadStream(filePath).pipe(res);
       return;
     } else if (stats.isDirectory()) {
-      const files = readdir(filePath);
+      const files =  await readdir(filePath);
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end(files.join(','));
     }
   } catch (err) {
+    console.log(err);
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end(`${filePath} is NOT FOUND`);
     return;
